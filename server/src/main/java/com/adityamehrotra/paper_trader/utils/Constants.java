ae555bc4a8bcee74283ff37785c07d71fc4c6f80
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-25 00:19:18 -0500

    Modified the Constants.java file

--- server/src/main/java/com/adityamehrotra/paper_trader/utils/Constants.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/utils/Constants.java
@@ -2,9 +2,6 @@ package com.adityamehrotra.paper_trader.utils;
 
 public class Constants {
 
-  private Constants() {
-  }
-
   public static final String USERNAME_NOT_FOUND = "Username not found: ";
 
   public static final String ID_NOT_FOUND = "ID not found: ";
