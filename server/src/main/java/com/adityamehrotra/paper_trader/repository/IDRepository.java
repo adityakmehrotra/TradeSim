e3c71ec2e7278b2d1054f2318bc12850a2d81da6
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-27 01:04:04 -0500

    Modified some files that were unnecessary.

--- server/src/main/java/com/adityamehrotra/paper_trader/repository/IDRepository.java
+++
@@ -1,8 +0,0 @@
-package com.adityamehrotra.paper_trader.repository;
-
-import com.adityamehrotra.paper_trader.model.PaperTraderID;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IDRepository extends MongoRepository<PaperTraderID, String> {}
