00a4ef4035021acd5ded3ccae522a32b43a1d1cf
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-26 23:14:49 -0500

    Modified the PolygonAPI Controller and Service

--- server/src/main/java/com/adityamehrotra/paper_trader/controller/PolygonAPIController.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/controller/PolygonAPIController.java
@@ -1,37 +1,28 @@
 package com.adityamehrotra.paper_trader.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.adityamehrotra.paper_trader.service.PolygonAPIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RequestMapping("/paper_trader/polygon")
 @RestController
 public class PolygonAPIController {
 
-  @Value("${apiKey}")
-  private String polygonApiKey;
+  private final PolygonAPIService polygonAPIService;
 
-  private final RestTemplate restTemplate;
-
-  public PolygonAPIController(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
+  public PolygonAPIController(PolygonAPIService polygonAPIService) {
+    this.polygonAPIService = polygonAPIService;
   }
 
   @GetMapping("/price")
   public ResponseEntity<String> getCurrentPrice(@RequestParam String ticker) {
-    String url =
-            String.format(
-                    "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/%s?apiKey=%s",
-                    ticker, polygonApiKey);
     try {
-      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-      return ResponseEntity.ok(response.getBody());
+      return polygonAPIService.getCurrentPrice(ticker);
     } catch (Exception e) {
       return ResponseEntity.status(500).body("Error fetching current price: " + e.getMessage());
     }
@@ -39,42 +30,28 @@ public class PolygonAPIController {
 
   @GetMapping("/news")
   public ResponseEntity<String> getCurrentNews(@RequestParam String ticker) {
-    String url =
-            String.format(
-                    "https://api.polygon.io/v2/reference/news?ticker=%s&order=desc&limit=50&apiKey=%s",
-                    ticker, polygonApiKey);
     try {
-      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-      return ResponseEntity.ok(response.getBody());
+      return polygonAPIService.getCurrentNews(ticker);
     } catch (Exception e) {
-      return ResponseEntity.status(500).body("Error fetching current price: " + e.getMessage());
+      return ResponseEntity.status(500).body("Error fetching current news: " + e.getMessage());
     }
   }
 
   @GetMapping("/keyStatistics")
   public ResponseEntity<String> getKeyStatistics(@RequestParam String ticker) {
-    String url =
-            String.format(
-                    "https://api.polygon.io/v2/aggs/ticker/%s/prev?adjusted=true&apiKey=%s",
-                    ticker, polygonApiKey);
     try {
-      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-      return ResponseEntity.ok(response.getBody());
+      return polygonAPIService.getKeyStatistics(ticker);
     } catch (Exception e) {
-      return ResponseEntity.status(500).body("Error fetching current price: " + e.getMessage());
+      return ResponseEntity.status(500).body("Error fetching key statistics: " + e.getMessage());
     }
   }
 
   @GetMapping("/companyData")
   public ResponseEntity<String> getCompanyData(@RequestParam String ticker) {
-    String url =
-            String.format(
-                    "https://api.polygon.io/v3/reference/tickers/%s?apiKey=%s", ticker, polygonApiKey);
     try {
-      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-      return ResponseEntity.ok(response.getBody());
+      return polygonAPIService.getCompanyData(ticker);
     } catch (Exception e) {
-      return ResponseEntity.status(500).body("Error fetching current price: " + e.getMessage());
+      return ResponseEntity.status(500).body("Error fetching company data: " + e.getMessage());
     }
   }
 
@@ -85,15 +62,10 @@ public class PolygonAPIController {
           @RequestParam String timespan,
           @RequestParam String from,
           @RequestParam String to) {
-    String url =
-            String.format(
-                    "https://api.polygon.io/v2/aggs/ticker/%s/range/%d/%s/%s/%s?adjusted=true&sort=asc&limit=50000&apiKey=%s",
-                    ticker, multiplier, timespan, from, to, polygonApiKey);
     try {
-      ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-      return ResponseEntity.ok(response.getBody());
+      return polygonAPIService.getChart(ticker, multiplier, timespan, from, to);
     } catch (Exception e) {
-      return ResponseEntity.status(500).body("Error fetching current price: " + e.getMessage());
+      return ResponseEntity.status(500).body("Error fetching chart data: " + e.getMessage());
     }
   }
 }
--- server/src/main/java/com/adityamehrotra/paper_trader/service/PolygonAPIService.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/service/PolygonAPIService.java
@@ -1,7 +1,53 @@
 package com.adityamehrotra.paper_trader.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PolygonAPIService {
+
+    @Value("${apiKey}")
+    private String polygonApiKey;
+
+    private final RestTemplate restTemplate;
+
+    public PolygonAPIService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseEntity<String> getCurrentPrice(String ticker) {
+        String url = String.format(
+                "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/%s?apiKey=%s",
+                ticker, polygonApiKey);
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> getCurrentNews(String ticker) {
+        String url = String.format(
+                "https://api.polygon.io/v2/reference/news?ticker=%s&order=desc&limit=50&apiKey=%s",
+                ticker, polygonApiKey);
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> getKeyStatistics(String ticker) {
+        String url = String.format(
+                "https://api.polygon.io/v2/aggs/ticker/%s/prev?adjusted=true&apiKey=%s",
+                ticker, polygonApiKey);
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> getCompanyData(String ticker) {
+        String url = String.format(
+                "https://api.polygon.io/v3/reference/tickers/%s?apiKey=%s", ticker, polygonApiKey);
+        return restTemplate.getForEntity(url, String.class);
+    }
+
+    public ResponseEntity<String> getChart(String ticker, Integer multiplier, String timespan, String from, String to) {
+        String url = String.format(
+                "https://api.polygon.io/v2/aggs/ticker/%s/range/%d/%s/%s/%s?adjusted=true&sort=asc&limit=50000&apiKey=%s",
+                ticker, multiplier, timespan, from, to, polygonApiKey);
+        return restTemplate.getForEntity(url, String.class);
+    }
 }
