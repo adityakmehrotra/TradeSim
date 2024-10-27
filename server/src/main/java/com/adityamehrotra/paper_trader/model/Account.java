781f654928dac06f600016ec82a0769f9b26e933
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-27 01:09:28 -0500

    Added annotations to the Account file.

--- server/src/main/java/com/adityamehrotra/paper_trader/model/Account.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/model/Account.java
@@ -1,6 +1,10 @@
 package com.adityamehrotra.paper_trader.model;
 
 import java.util.List;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +18,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ToString
 @Document(collection = "Paper Trader- Account")
 public class Account {
-  @Id private Integer accountID;
+  @Id
+  @NotNull(message = "Account ID cannot be null")
+  private Integer accountID;
+
+  @NotEmpty(message = "First name cannot be empty")
   private String firstName;
+
+  @NotEmpty(message = "Last name cannot be empty")
   private String lastName;
+
+  @Email(message = "Email address should be valid")
+  @NotEmpty(message = "Email address cannot be empty")
   private String emailAddress;
+
+  @NotEmpty(message = "Password cannot be empty")
   private String password;
+
   private List<Integer> portfolioList;
 }
