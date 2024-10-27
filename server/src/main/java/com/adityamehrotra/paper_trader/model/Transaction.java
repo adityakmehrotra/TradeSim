2bfd8491eec99b9279f3ad4ddba4b2ff93db84bb
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-27 01:22:30 -0500

    Added annotations to the Transaction file.

--- server/src/main/java/com/adityamehrotra/paper_trader/model/Transaction.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/model/Transaction.java
@@ -1,5 +1,8 @@
 package com.adityamehrotra.paper_trader.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +16,33 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ToString
 @Document(collection = "Paper Trader- Transaction")
 public class Transaction {
-  @Id private Integer transactionID;
+  @Id
+  @NotNull(message = "Transaction ID cannot be null")
+  private Integer transactionID;
+
+  @NotNull(message = "Portfolio ID cannot be null")
   private Integer portfolioID;
+
+  @NotNull(message = "Account ID cannot be null")
   private Integer accountID;
+
+  @NotEmpty(message = "Order type cannot be empty")
   private String orderType;
-  private String gmtTime;
+
+  @NotEmpty(message = "Security code cannot be empty")
   private String securityCode;
+
+  @NotEmpty(message = "GMT time cannot be empty")
+  private String gmtTime;
+
+  @NotNull(message = "Share amount cannot be null")
+  @Min(value = 0, message = "Share amount must be non-negative")
   private Double shareAmount;
+
+  @NotNull(message = "Cash amount cannot be null")
+  @Min(value = 0, message = "Cash amount must be non-negative")
   private Double cashAmount;
+
+  @Min(value = 0, message = "Current price must be non-negative")
   private Double currPrice;
 }
