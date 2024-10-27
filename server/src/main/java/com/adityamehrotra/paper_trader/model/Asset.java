8f1f161fa5a1f7d823f922185db642bb25d3a51c
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-10-27 01:07:10 -0500

    Added annotations to the Asset file.

--- server/src/main/java/com/adityamehrotra/paper_trader/model/Asset.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/model/Asset.java
@@ -1,5 +1,7 @@
 package com.adityamehrotra.paper_trader.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ToString
 @Document(collection = "Paper Trader- Asset")
 public class Asset {
+  @NotNull(message = "Shares owned cannot be null")
+  @Min(value = 0, message = "Shares owned must be greater than or equal to 0")
   private Double sharesOwned;
+
+  @NotNull(message = "Initial cash investment cannot be null")
+  @Min(value = 0, message = "Initial cash investment must be non-negative")
   private Double initialCashInvestment;
+
+  @NotNull(message = "GMT purchased time cannot be null")
   private String gmtPurchased;
+
+  @NotNull(message = "Initial price cannot be null")
+  @Min(value = 0, message = "Initial price must be non-negative")
   private Double initPrice;
 }
