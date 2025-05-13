86713cda83900f3c879c3e25965fb5c50ea0a6a2
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-13 18:22:12 -0500

    Updated the main.jsx file.

--- client_new/src/main.jsx
+++ client_new/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './context/AuthContext';
+import { StockContext, StockProvider } from './context/StockContext';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <StockProvider>
+        <App />
+      </StockProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
