0ed9551057e083621eb290a77918214d450b815b
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-15 15:43:44 -0500

    Added the Asset model with data validation annotations.

---
+++ server/src/main/java/com/adityamehrotra/paper_trader/model/Asset.java
@@ -0,0 +1,29 @@
+package com.adityamehrotra.paper_trader.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "TradeSim-Asset")
+public class Asset {
+    @Id
+    @NotNull(message = "Asset ID cannot be null")
+    @Min(value = 1, message = "Asset ID must be greater than 0")
+    private Integer assetID;
+    
+    @NotEmpty(message = "Ticker cannot be empty")
+    private String ticker;
+
+    @NotEmpty(message = "Name cannot be empty")
+    private String name;
+}
