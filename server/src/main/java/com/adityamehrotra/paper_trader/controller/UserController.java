c2a9428d328ae9d0d28028d8c80b0766de162730
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-15 23:36:59 -0500

    Created the UserController class.

---
+++ server/src/main/java/com/adityamehrotra/paper_trader/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.adityamehrotra.paper_trader.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/tradesim/api/user")
+public class UserController {
+    
+}
