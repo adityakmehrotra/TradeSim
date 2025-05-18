2a6fc813c1d25d3effc898922fc90e1877bab8f8
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-18 01:21:29 -0500

    Updated the UserRepository file.

--- server/src/main/java/com/adityamehrotra/paper_trader/repository/UserRepository.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.adityamehrotra.paper_trader.repository;
 
 import com.adityamehrotra.paper_trader.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Integer> {}
+public interface UserRepository extends MongoRepository<User, Integer> {
+    boolean existsByEmail(@Email(message = "Email address should be valid") @NotEmpty(message = "Email address cannot be empty") String email);
+    boolean existsByUsername(@NotEmpty(message = "Username cannot be empty") String username);
+    User findByEmail(@Email(message = "Email address should be valid") @NotEmpty(message = "Email address cannot be empty") String email);
+    User findByUsername(@NotEmpty(message = "Username cannot be empty") String username);
+    User findByUserID(@NotEmpty(message = "User ID cannot be empty") Integer userID);
+    int getNextID();
+}
