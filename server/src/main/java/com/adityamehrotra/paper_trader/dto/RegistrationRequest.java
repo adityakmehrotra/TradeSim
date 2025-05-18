ae59103320877c0c811639cb728fe78c53ec00d8
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-18 01:22:40 -0500

    Update the RegistrationRequest file.

--- server/src/main/java/com/adityamehrotra/paper_trader/dto/RegistrationRequest.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/dto/RegistrationRequest.java
@@ -1,4 +1,65 @@
 package com.adityamehrotra.paper_trader.dto;
 
+import java.util.Date;
+
 public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private Date dateCreated;
+
+    public RegistrationRequest(String firstName, String lastName, String username, String password, String email, Date dateCreated) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
 }
