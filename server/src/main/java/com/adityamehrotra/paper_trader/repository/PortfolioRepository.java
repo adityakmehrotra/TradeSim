0b84209c98f252943a1cdd4d4823a53445ba03ff
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2025-05-20 19:13:04 -0500

    Added findByPortfolioID method to PortfolioRepository.

--- server/src/main/java/com/adityamehrotra/paper_trader/repository/PortfolioRepository.java
+++ server/src/main/java/com/adityamehrotra/paper_trader/repository/PortfolioRepository.java
@@ -1,8 +1,12 @@
 package com.adityamehrotra.paper_trader.repository;
 
 import com.adityamehrotra.paper_trader.model.Portfolio;
+import com.adityamehrotra.paper_trader.model.User;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortfolioRepository extends MongoRepository<Portfolio, Integer> {}
+public interface PortfolioRepository extends MongoRepository<Portfolio, Integer> {
+    Portfolio findByPortfolioID(@NotEmpty(message = "Portfolio ID cannot be empty") Integer portfolioID);
+}
